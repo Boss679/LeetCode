fb7f50e35d267fea66769d0c7f9a95385d4516e5
N J <55899982+Boss679@users.noreply.github.com>
2024-03-29 15:48:23 -0400

    Time: 408 ms (87.36%) | Memory: 32 MB (32.02%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,9 +1,24 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        Determines if the given list of integers contains any duplicates.
+
+        Args:
+            nums (List[int]): The list of integers to be checked for duplicates.
+
+        Returns:
+            bool: True if there are any duplicates in the list, False otherwise.
+        """
+
+        # Create an empty set to store unique integers encountered so far.
         hashet = set()
 
+        # Iterate through the list of integers.
         for n in nums:
+            # If the integer is already in the set, it's a duplicate.
             if n in hashet:
                 return True
+            # Otherwise, add it to the set.
             hashet.add(n)
+        # If no duplicates were found, return False.
         return False
